41d50f869fac752430dd8c7e03a4340c1f188dc4
Aneesha Jose <aneesha.j@servify.in>
2020-03-22 18:38:21 +0530

    -- setup :: setting up qualifiers and scopes for dependency injection

---
+++ app/src/main/java/com/aneeshajose/trending/base/qualifiers/ContextQualifiers.kt
@@ -0,0 +1,16 @@
+package com.aneeshajose.trending.base.qualifiers
+
+import javax.inject.Qualifier
+
+/**
+ * Created by Aneesha Jose on 2020-03-22.
+ * Class that contains the qualifier definitions for context types
+ */
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class ApplicationContext
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class ActivityContext
---
+++ app/src/main/java/com/aneeshajose/trending/base/scopes/Scopes.kt
@@ -0,0 +1,23 @@
+package com.aneeshajose.trending.base.scopes
+
+import javax.inject.Scope
+
+/**
+ * Created by Aneesha Jose on 2020-03-22.
+ */
+
+@Retention(AnnotationRetention.BINARY)
+@Scope
+annotation class ApplicationScope
+
+@Retention(AnnotationRetention.BINARY)
+@Scope
+annotation class ActivityScope
+
+@Retention(AnnotationRetention.BINARY)
+@Scope
+annotation class BaseActivityScope
+
+@Retention(AnnotationRetention.BINARY)
+@Scope
+annotation class FragmentScope
