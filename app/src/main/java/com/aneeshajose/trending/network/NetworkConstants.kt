5daf71d702fdce84f2e97200258bd07909cc5cde
Aneesha Jose <aneesha.j@servify.in>
2020-03-22 14:27:20 +0530

    -- setup :: base url constant created as a network constant

---
+++ app/src/main/java/com/aneeshajose/trending/network/NetworkConstants.kt
@@ -0,0 +1,8 @@
+package com.aneeshajose.trending.network
+
+/**
+ * Created by Aneesha Jose on 2020-03-22.
+ * A central place to define network related constants used throughout the application
+ */
+
+const val BASE_URL = "https://github-trending-api.now.sh/"
